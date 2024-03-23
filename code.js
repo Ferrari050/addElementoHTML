2b0987ab9ab441d709efeb2d4c73e41587ea5822
Ferrari050 <jvm.ferrari@hotmail.com>
2024-03-23 18:25:11 -0300

    finalizado

--- code.js
+++ code.js
@@ -1,12 +1,16 @@
 function adicionarItem() {
-    let listItem = document.createElement("li");
-    listItem.className = "listModule";
-    let text = document.createTextNode(" ");
-    listItem.appendChild(text);
-    let list = document.querySelector(".list");
-    list.appendChild(listItem);
+  let listItem = document.createElement("li");
+  listItem.className = "listModule";
+  let text = document.createTextNode(" ");
+  listItem.appendChild(text);
+  let list = document.querySelector(".list");
+  list.appendChild(listItem);
 }
 
 const addButton = document.getElementById("addButton");
+const reloadButton = document.getElementById("resetButton");
 
 addButton.addEventListener("click", adicionarItem);
+reloadButton.addEventListener("click", () => {
+  window.location.reload();
+});
